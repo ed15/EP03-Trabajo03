549c886e85ea188af41a456d65069840c513ef04
ed15 <ersg90z@gmail.com>
2016-03-03 16:46:07 -0500

    EP03-Trabajo03

---
+++ galones_litros/js/galones_litros.js
@@ -0,0 +1,9 @@
+window.onload = function()
+{
+		alert("¿Convierte galones en litros?");
+        var galon;
+        galon=prompt("Digite el número de galones");
+        litro=(galon *3.7854);
+        alert("Sus Litros: " + litro);
+
+}
---
+++ gravedad_luna/js/gravedad.js
@@ -0,0 +1,9 @@
+window.onload = function()
+{
+	alert("¿Cual es su peso efectivo en la luna?");
+	var peso;
+	peso=prompt("Escriba su peso");
+	pesoLuna=(peso * 1.62);
+	alert("Su peso en la luna es de: " + pesoLuna);
+
+}
